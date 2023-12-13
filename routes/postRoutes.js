398f170a3f360f166eb7040064166bdf5c4fa36c
khalil <salamikhalil02@gmail.com>
2023-12-13 07:41:01 +0100

    commented out unsed code

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,7 +5,7 @@ const { getAllPost, createPost } = require("../controllers/postController");
 
 router.route("/").get(getAllPost).post(createPost);
 
-router.get("/", getAllPost);
-router.post("/", createPost);
+// router.get("/:id", getAllPost);
+// router.post("/", createPost);
 
 module.exports = router;
